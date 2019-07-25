aa07286a00d651f5e8f483cc1076ce057b6c16e8
marianneji <marianneji@gmail.com>
2019-07-25 13:00:26 +0200

    codable protocol, with the results of petition

---
+++ Json/Petitions.swift
@@ -0,0 +1,14 @@
+//
+//  Petitions.swift
+//  Json
+//
+//  Created by Graphic Influence on 23/07/2019.
+//  Copyright © 2019 marianne massé. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    
+    var results: [Petition]
+}
